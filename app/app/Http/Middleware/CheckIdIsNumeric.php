35448fa2073d6dce0561abe16111c80b6713d3d5
Pavel-Zhukoff <pavel.zhuko.ff@yandex.ru>
2021-01-02 21:26:18 +0300

    fix id check and add PHPDoc

--- app/app/Http/Middleware/CheckIdIsNumeric.php
+++ app/app/Http/Middleware/CheckIdIsNumeric.php
@@ -9,11 +9,19 @@ use Illuminate\Http\Request;
 
 class CheckIdIsNumeric
 {
+    /**
+     * Check if requested `id` is integer
+     *
+     * @param Request $request
+     * @param Closure $next
+     * @param int $index
+     * @return \Illuminate\Http\JsonResponse|mixed
+     */
     public function handle(Request $request, Closure $next, int $index) {
         $id = $request->segment($index);
-        if (!is_numeric($id))
+        if (!is_numeric($id) && strpos($id, '.') !== false)
         {
-            return response()->json(['error' => 'ID должен быть целым числом!']);
+            return response()->json(['error' => 'ID должен быть целым числом!'], 400);
         }
         return $next($request);
     }
